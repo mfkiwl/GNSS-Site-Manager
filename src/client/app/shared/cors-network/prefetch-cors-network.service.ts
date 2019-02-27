2742c4502cd09c3ec55bec0abe9f543c358ef4eb
Frank Fu <frank.fu@ga.gov.au>
2019-02-28 10:18:38 +1100

    Fix an import issue in PrefetchCorsNetworkResolver class

--- src/client/app/shared/cors-network/prefetch-cors-network.service.ts
+++ src/client/app/shared/cors-network/prefetch-cors-network.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Router, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import { DialogService, CorsNetworkService } from '../index';
+import { DialogService } from '../global/dialog.service';
+import { CorsNetworkService } from './cors-network.service';
 import { CorsNetworkModel } from './cors-network-model';
 
 @Injectable()
-export class PrefetchCorsNetworkResolver implements Resolve<CorsNetworkModel> {
+export class PrefetchCorsNetworkResolver implements Resolve<CorsNetworkModel[]> {
 
-    constructor(private router: Router,
-                private dialogService: DialogService,
+    constructor(private dialogService: DialogService,
                 private corsNetworkService: CorsNetworkService) {
     }
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<CorsNetworkModel> {
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<CorsNetworkModel[]> {
         return this.corsNetworkService.getAllCorsNetworks()
             .catch((error: any): any => {
                 this.dialogService.showErrorMessage('Warning: no CORS networks found.');
