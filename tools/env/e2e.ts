ae5796674e031e3a5aeaacbf2a2d7f73dab234f4
Frank Fu <frank.fu@ga.gov.au>
2019-05-01 14:30:58 +1000

    Switch e2e resources from TEST to DEV environment

--- tools/env/e2e.ts
+++ tools/env/e2e.ts
@@ -2,13 +2,13 @@ import { EnvConfig } from './env-config.interface';
 
 const E2EConfig: EnvConfig = {
   ENV: 'E2E',
-  WEB_SERVICE_URL: 'https://testgeodesy-webservices.geodesy.ga.gov.au',
-  WFS_GEOSERVER_URL: 'https://testgeodesy-geoserver.geodesy.ga.gov.au/geoserver/wfs',
-  OPENAM_SERVER_URL: 'https://testgeodesy-openam.geodesy.ga.gov.au/openam',
+  WEB_SERVICE_URL: 'https://devgeodesy-webservices.geodesy.ga.gov.au',
+  WFS_GEOSERVER_URL: 'https://devgeodesy-geoserver.geodesy.ga.gov.au/geoserver/wfs',
+  OPENAM_SERVER_URL: 'https://devgeodesy-openam.geodesy.ga.gov.au/openam',
   CLIENT_URL: 'http://localhost:5555',
   WEB_URL_LINKS: [
     {name: 'AUSCORS', url: 'https://www.auscors.ga.gov.au'},
-    {name: 'GNSS Data Repository', url: 'https://test-data.gnss.ga.gov.au'},
+    {name: 'GNSS Data Repository', url: 'https://dev-data.gnss.ga.gov.au'},
     {name: 'GNSS Portal', url: 'https://portal.gnss.ga.gov.au'},
   ],
 };
